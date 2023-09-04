4d0e391f2da6aa1b455c6061da82b8e57e3f697a
Fekadu Tadesse <aaumsccs@gmail.com>
2023-09-04 12:44:34 +0300

    Starting commenting
    replacing fake genreService and fake movies to the real one
    
    end of commenting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
-import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getMovies, deleteMovie } from "../services/movieService";
+import { getGenres } from "../services/genreService";
 import { paginate } from "../utils/paginate";
 import MoviesTable from "./moviesTable";
 import { Link } from "react-router-dom";
 import SearchBox from "./searchBox";
+import { toast } from "react-toastify";
 import _ from "lodash";
 
 class Movies extends Component {
@@ -21,14 +22,27 @@ class Movies extends Component {
     selectedGenre: null,
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres });
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ _id: "", name: "All Genres" }, ...data];
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = (movie) => {
-    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+  handleDelete = async (movie) => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error("This movie has already been deleted.");
+
+      this.setState({ movies: originalMovies });
+    }
   };
 
   handleLike = (movie) => {
@@ -72,11 +86,6 @@ class Movies extends Component {
       );
     else if (selectedGenre && selectedGenre._id)
       filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
-
-    // const filtered =
-    //   selectedGenre && selectedGenre._id
-    //     ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-    //     : allMovies;
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
---
+++ src/services/httpService.js
@@ -0,0 +1,26 @@
+import Axios from "axios";
+import { toast } from "react-toastify";
+import logger from "./logService";
+
+Axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error("An unexpected error occurred.");
+  }
+  console.log("INTERCEPTOR CALLED");
+
+  return Promise.reject(error);
+});
+
+// eslint-disable-next-line
+export default {
+  get: Axios.get,
+  post: Axios.post,
+  put: Axios.put,
+  delete: Axios.delete,
+};
